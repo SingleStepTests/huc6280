fbc00d80abde74ada289e4c28421976faa6ffef6
raddad772 <81476102+raddad772@users.noreply.github.com>
2025-06-28 14:50:25 -0400

    v1 only 1 test

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # huc6280
 Mesen-generated Huc6280 JSON single-step tests
+
+Mesen *may* be the most accurate Huc6280 emulator currently. It passes timing tests other emulators don't. It needs more research, though, and these tests are only as good as Mesen.
+
+These tests are not perfect. They treat RAM as a flat 2MB chunk, with no MMU other than the usual MPR translation. I wasn't clear on if "dummy reads" and "dummy writes" actually happened, so I just included a "dummy pin" indicating if a cycle was either.
+
+The tests are distributed in a binary format, because it's easier for me to use them this way. However, I include a Python script to translate them to plain .json. If you'd like to use the binary format, follow what the Python transcoder program does to extract the data.
+
+Here is an example test:
+
+```json
+{
+  "name": "01 #0",
+  "opcode": 1,
+  "initial": {
+    "A": 177,
+    "X": 82,
+    "Y": 20,
+    "S": 118,
+    "P": 79,
+    "PC": 9336,
+    "MPR": [ 235, 126, 85, 150, 189, 131, 92, 110 ],
+    "RAM": [
+      [ 1033336, 1 ],
+      [ 1033337, 153 ],
+      [ 1032427, 254 ],
+      [ 1032428, 19 ],
+      [ 1930238, 47 ]
+    ]
+  },
+  "final": {
+    "A": 191,
+    "X": 82,
+    "Y": 20,
+    "S": 118,
+    "P": 205,
+    "PC": 9338,
+    "MPR": [ 235, 126, 85, 150, 189, 131, 92, 110 ],
+    "RAM": [
+      [ 1033336, 1 ],
+      [ 1033337, 153 ],
+      [ 1032427, 254 ],
+      [ 1032428, 19 ],
+      [ 1930238, 47 ]
+    ]
+  },
+  "cycles": [
+    [ 1033336, 1, "r--" ],
+    [ 1033337, 153, "r--" ],
+    [ 2097151, 255, "r-d" ],
+    [ 1032427, 254, "r--" ],
+    [ 1032428, 19, "r--" ],
+    [ 2097151, 255, "r-d" ],
+    [ 1930238, 47, "r--" ]
+  ]
+}
+
+```
+
+### The top-level keys are "initial," "final," "opcode," "cycles," and "name."
+
+### name
+A unique name for the test. Also used as the RNG seed for the test.
+
+### opcode
+The opcode being tested
+
+### Initial and final are the same format.
+They contain the initial and final state of all relevant registers in the CPU, as well as the state of various places in RAM.
+
+A, X, Y, S, P, and PC should be self-explanatory, as should MPR.
+
+Each 2-pair in the "RAM" section (example):
+```json
+[ 1033337, 153 ]
+```
+consists of an address and value.
+
+
+### cycles
+A list of cycles, assuming that opcode fetch is the first cycle (many emulators use that as the last cycle of the previous instruction).
+
+Example:
+```json
+[ 2097151, 255, "r-d" ]
+```
+
+This is address pins, data pins, and r/w/dummy pins. A pin is 'on' if it is present in the list, and 'off' if it is -.
+
+The read and write pins should be obvious. The dummy pins are to indicate a "dummy memory access." I suspect these don't even make it to the bus on a real Huc6280, but it's possible they do, the same as a 6502.
+
+## Disclaimers
+
+* Again, these tests are made from Mesen, so are only as accurate as Mesen is.
+* We're not even 100% sure about dummy reads and writes how they act...
+* So buyer beware!
+
+I hope you find them useful!
---
+++ transcode_json.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+import os
+import glob
+import json
+from struct import unpack_from
+from typing import Dict, Any
+
+
+def load_cycles(buf, ptr):
+    cycles = []
+    full_sz = 4
+    num_cycles = unpack_from('I', buf, ptr)[0]
+    ptr += 4
+    for i in range(0, num_cycles):
+        cdata = unpack_from('I', buf, ptr)[0]
+        ptr += 4
+        full_sz += 4
+        # u32 o = c->RD | (c->WR << 1) | (c->Addr << 2) | (c->D << 24);
+        rd = cdata & 1
+        wr = (cdata >> 1) & 1
+        dummy = (cdata >> 2) & 1
+        addr = (cdata >> 3) & 0x1FFFFF
+        data = (cdata >> 24) & 0xFF
+        pstr = 'r' if rd else '-'
+        pstr += 'w' if wr else '-'
+        pstr += 'd' if dummy else '-'
+        cycles.append([addr, data, pstr])
+
+    return full_sz, cycles
+
+
+def load_state(buf, ptr) -> (int, Any):
+    full_sz = unpack_from('i', buf, ptr)[0]
+    ptr += 4
+    '''
+        W32(st.num_RAM);
+        for (u32 i = 0; i < st.num_RAM; i++) {
+            struct RAM_pair *rp = &st.RAM_pairs[i];
+            W16(rp->addr);
+            W8(rp->val);
+        }
+        size_t len = ts.ptr - bufbegin;
+        cW32(bufbegin, 0, (u32)len);
+    
+    '''
+    state = {}
+    values = unpack_from('B' * 5, buf, ptr)
+    ptr += 5
+    state['A'] = values[0]
+    state['X'] = values[1]
+    state['Y'] = values[2]
+    state['S'] = values[3]
+    state['P'] = values[4]
+    state['PC'] = unpack_from('H', buf, ptr)[0]
+    ptr += 2
+    state['MPR'] = []
+    for i in range(0, 8):
+        state['MPR'].append(unpack_from('B', buf, ptr)[0])
+        ptr += 1
+    state['RAM'] = []
+    num_ram = unpack_from('i', buf, ptr)[0]
+    ptr += 4
+    for i in range(0, num_ram):
+        addr = unpack_from('I', buf, ptr)[0]
+        ptr += 4
+        val = unpack_from('B', buf, ptr)[0]
+        ptr += 1
+        state['RAM'].append((addr, val))
+    return full_sz, state
+
+
+def decode_test(buf, ptr) -> (int, Dict):
+    full_sz = unpack_from('i', buf, ptr)[0]
+    ptr += 4
+    test = {}
+    # 50 char name
+    # 32-bit opcode
+    # initial, final
+    # num. cycles
+    # cycle array
+    nn = unpack_from('50s', buf, ptr)[0].decode()
+    test['name'] = nn.strip()
+
+    sz = 50
+    ptr += sz
+    opcode = unpack_from('I', buf, ptr)[0]
+    test['opcode'] = opcode
+    ptr += 4
+    sz, test['initial'] = load_state(buf, ptr)
+    ptr += sz
+    sz, test['final'] = load_state(buf, ptr)
+    ptr += sz
+    sz, test['cycles'] = load_cycles(buf, ptr)
+    ptr += sz
+
+    #print(json.dumps(test, indent=2))
+
+    return full_sz, test
+
+
+def decode_file(infilename, outfilename):
+    print('Decoding ' + infilename)
+    with open(infilename, 'rb') as infile:
+        content = infile.read()
+    NUMTESTS = unpack_from('<I', content, 0)[0]
+    ptr = 4
+    tests = []
+    for i in range(0, NUMTESTS):
+        sz, test = decode_test(content, ptr)
+        ptr += sz
+        tests.append(test)
+    if os.path.exists(outfilename):
+        os.unlink(outfilename)
+    with open(outfilename, 'w') as outfile:
+        outfile.write(json.dumps(tests, indent=2))
+
+
+def do_path(where):
+    print("Doing path...", where)
+    fs = glob.glob(where + '**.json.bin')
+    for fname in fs:
+        decode_file(fname, fname[:-4])
+
+
+def main():
+    do_path('v1/')
+
+
+if __name__ == '__main__':
+    main()
